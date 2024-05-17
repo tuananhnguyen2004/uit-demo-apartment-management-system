34f1c588ee94b0a7de841dff293a548b94a34499
chitken-dev <lethianhxuan1014@gmail.com>
2024-05-17 23:50:02 +0700

    completed quanlythietbi ui designs: FormThietBi, FormThemTB, and FormSuaTB

---
+++ QuanLyChungCu/src/QuanLyThietBi/TableActionCellEditor.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package QuanLyThietBi;
+
+import java.awt.Component;
+import javax.swing.DefaultCellEditor;
+import javax.swing.JCheckBox;
+import javax.swing.JTable;
+
+/**
+ *
+ * @author lethi
+ */
+public class TableActionCellEditor extends DefaultCellEditor {
+
+    public TableActionCellEditor() {
+        super(new JCheckBox());
+    }
+
+    
+    @Override
+    public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row, int column) {
+        PanelAction action = new PanelAction();
+        action.setBackground(table.getSelectionBackground());
+        return action;
+    }
+    
+    
+}
