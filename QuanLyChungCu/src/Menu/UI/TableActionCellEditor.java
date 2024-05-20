cd38d0dffd653ca29b55a167eca81d69cae0b53d
thiennh1 <huuthien180204@gmail.com>
2024-05-20 20:16:08 +0700

    Them FormNhaCungCap

---
+++ QuanLyChungCu/src/Menu/UI/TableActionCellEditor.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package raven.cell;
+
+import java.awt.Component;
+import javax.swing.DefaultCellEditor;
+import javax.swing.JCheckBox;
+import javax.swing.JTable;
+
+/**
+ *
+ * @author Thien Nguyen
+ */
+public class TableActionCellEditor extends DefaultCellEditor {
+
+    public TableActionCellEditor() {
+        super(new JCheckBox());
+    }
+
+    @Override
+    public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row, int column) {
+        PanelAction action = new PanelAction();
+        action.setBackground(table.getSelectionBackground());
+        return action;
+    }
+}
